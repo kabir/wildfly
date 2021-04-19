b1208ce3969756b0ff60e5347c6fe21760ff37d6
Kabir Khan <kkhan@redhat.com>
2021-04-19 19:11:37 +0100

    [WFLY-14715] Add MP Context Propagation

---
+++ microprofile/context-propagation-smallrye/extension/src/main/java/org/wildfly/extension/microprofile/context/propagation/providers/ServletRequestContextProvider.java
@@ -0,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.extension.microprofile.context.propagation.providers;
+
+import java.util.Map;
+
+import org.eclipse.microprofile.context.spi.ThreadContextProvider;
+import org.eclipse.microprofile.context.spi.ThreadContextSnapshot;
+
+import io.undertow.servlet.handlers.ServletRequestContext;
+
+/**
+ * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
+ */
+public class ServletRequestContextProvider implements ThreadContextProvider {
+
+    // TODO this should be enabled in the ServiceLoader file once it works
+    // Probably it should be in its own module so we don't HAVE to depend on the web layer
+
+    @Override
+    public ThreadContextSnapshot currentContext(Map<String, String> props) {
+        ServletRequestContext captured = ServletRequestContext.current();
+        return () -> {
+            ServletRequestContext current = restore(captured);
+            return () -> restore(current);
+        };
+    }
+
+    private ServletRequestContext restore(ServletRequestContext context) {
+        ServletRequestContext currentContext = ServletRequestContext.current();
+        if (context == null)
+            ServletRequestContext.clearCurrentServletAttachments();
+        else
+            ServletRequestContext.setCurrentRequestContext(context);
+        return currentContext;
+    }
+
+    @Override
+    public ThreadContextSnapshot clearedContext(Map<String, String> props) {
+        return () -> {
+            ServletRequestContext current = restore(null);
+            return () -> restore(current);
+        };
+    }
+
+    @Override
+    public String getThreadContextType() {
+        return "Servlet";
+    }
+}
