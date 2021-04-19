bb1934b4c2adf38414c353199f112f61f249df4e
Kabir Khan <kkhan@redhat.com>
2021-04-19 19:11:37 +0100

    [WFLY-14715] Add MP Context Propagation

--- testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/EnableReactiveExtensionsSetupTask.java
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/EnableReactiveExtensionsSetupTask.java
@@ -34,8 +34,15 @@ import org.jboss.dmr.ModelNode;
 public class EnableReactiveExtensionsSetupTask extends CLIServerSetupTask {
     private static final String MODULE_REACTIVE_MESSAGING = "org.wildfly.extension.microprofile.reactive-messaging-smallrye";
     private static final String MODULE_REACTIVE_STREAMS_OPERATORS = "org.wildfly.extension.microprofile.reactive-streams-operators-smallrye";
+    private static final String MODULE_CONTEXT_PROPAGATION = "org.wildfly.extension.microprofile.context-propagation-smallrye";
     private static final String SUBSYSTEM_REACTIVE_MESSAGING = "microprofile-reactive-messaging-smallrye";
     private static final String SUBSYSTEM_REACTIVE_STREAMS_OPERATORS = "microprofile-reactive-streams-operators-smallrye";
+    private static final String SUBSYSTEM_CONTEXT_PROPAGATION = "microprofile-context-propagation-smallrye";
+    public static final String EXTENSION_ADD = "/extension=%s:add";
+    public static final String EXTENSION_REMOVE = "/extension=%s:remove";
+    public static final String SUBSYSTEM_ADD = "/subsystem=%s:add";
+    public static final String SUBSYSTEM_REMOVE = "/subsystem=%s:remove";
+
 
     public EnableReactiveExtensionsSetupTask() {
     }
@@ -43,34 +50,48 @@ public class EnableReactiveExtensionsSetupTask extends CLIServerSetupTask {
     @Override
     public void setup(ManagementClient managementClient, String containerId) throws Exception {
         boolean rsoExt = !containsChild(managementClient, "extension", MODULE_REACTIVE_STREAMS_OPERATORS);
-        boolean rsoSs = !containsChild(managementClient, "extension", SUBSYSTEM_REACTIVE_STREAMS_OPERATORS);
+        boolean rsoSs = !containsChild(managementClient, "subsystem", SUBSYSTEM_REACTIVE_STREAMS_OPERATORS);
         boolean rmExt = !containsChild(managementClient, "extension", MODULE_REACTIVE_MESSAGING);
-        boolean rmSs = !containsChild(managementClient, "extension", SUBSYSTEM_REACTIVE_MESSAGING);
+        boolean rmSs = !containsChild(managementClient, "subsystem", SUBSYSTEM_REACTIVE_MESSAGING);
+        boolean cpExt = !containsChild(managementClient, "extension", MODULE_CONTEXT_PROPAGATION);
+        boolean cpSs = !containsChild(managementClient, "subsystem", SUBSYSTEM_CONTEXT_PROPAGATION);
 
         NodeBuilder nb = this.builder.node(containerId);
         if (rsoExt) {
-            nb.setup("/extension=%s:add", MODULE_REACTIVE_STREAMS_OPERATORS);
+            nb.setup(EXTENSION_ADD, MODULE_REACTIVE_STREAMS_OPERATORS);
+        }
+        if (cpExt) {
+            nb.setup(EXTENSION_ADD, MODULE_CONTEXT_PROPAGATION);
         }
         if (rmExt) {
-            nb.setup("/extension=%s:add", MODULE_REACTIVE_MESSAGING);
+            nb.setup(EXTENSION_ADD, MODULE_REACTIVE_MESSAGING);
         }
         if (rsoSs) {
-            nb.setup("/subsystem=%s:add", SUBSYSTEM_REACTIVE_STREAMS_OPERATORS);
+            nb.setup(SUBSYSTEM_ADD, SUBSYSTEM_REACTIVE_STREAMS_OPERATORS);
+        }
+        if (cpSs) {
+            nb.setup(SUBSYSTEM_ADD, SUBSYSTEM_CONTEXT_PROPAGATION);
         }
         if (rmSs) {
-            nb.setup("/subsystem=%s:add", SUBSYSTEM_REACTIVE_MESSAGING);
+            nb.setup(SUBSYSTEM_ADD, SUBSYSTEM_REACTIVE_MESSAGING);
         }
         if (rmSs) {
-            nb.teardown("/subsystem=%s:remove", SUBSYSTEM_REACTIVE_MESSAGING);
+            nb.teardown(SUBSYSTEM_REMOVE, SUBSYSTEM_REACTIVE_MESSAGING);
+        }
+        if (cpSs) {
+            nb.teardown(SUBSYSTEM_REMOVE, SUBSYSTEM_CONTEXT_PROPAGATION);
         }
         if (rsoSs) {
-            nb.teardown("/subsystem=%s:remove", SUBSYSTEM_REACTIVE_STREAMS_OPERATORS);
+            nb.teardown(SUBSYSTEM_REMOVE, SUBSYSTEM_REACTIVE_STREAMS_OPERATORS);
         }
         if (rmExt) {
-            nb.teardown("/extension=%s:remove", MODULE_REACTIVE_MESSAGING);
+            nb.teardown(EXTENSION_REMOVE, MODULE_REACTIVE_MESSAGING);
+        }
+        if (cpSs) {
+            nb.teardown(EXTENSION_REMOVE, MODULE_CONTEXT_PROPAGATION);
         }
         if (rsoSs) {
-            nb.teardown("/extension=%s:remove", MODULE_REACTIVE_STREAMS_OPERATORS);
+            nb.teardown(EXTENSION_REMOVE, MODULE_REACTIVE_STREAMS_OPERATORS);
         }
         super.setup(managementClient, containerId);
     }
