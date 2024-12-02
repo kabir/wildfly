0c774c6fb220d0c663085a630346604f2aff2235
Kabir Khan <kkhan@redhat.com>
2024-12-02 21:12:02 +0000

    WIP fix kafka on podman

--- testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/RunKafkaSetupTask.java
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/RunKafkaSetupTask.java
@@ -5,23 +5,45 @@
 
 package org.wildfly.test.integration.microprofile.reactive;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_RESOURCE_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SYSTEM_PROPERTY;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+
+import java.io.IOException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import io.smallrye.reactive.messaging.kafka.companion.KafkaCompanion;
 import org.jboss.arquillian.testcontainers.api.DockerRequired;
 import org.jboss.as.arquillian.api.ServerSetupTask;
 import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.operations.common.Util;
+import org.jboss.dmr.ModelNode;
+import org.junit.Assert;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.wildfly.security.manager.WildFlySecurityManager;
 
 @DockerRequired
 public class RunKafkaSetupTask implements ServerSetupTask {
-    volatile KafkaContainer container;
-    volatile KafkaCompanion companion;
+    private volatile KafkaContainer container;
+    private volatile KafkaCompanion companion;
+
+    private static final String KAFKA_PORT = "org.wildfly.test.kafka.port";
+    private static final PathAddress KAFKA_PORT_PROPERTY_ADDR = PathAddress.pathAddress(SYSTEM_PROPERTY, KAFKA_PORT);
+//    private static final PathAddress BOOTSTRAP_SERVERS_PROPERTY_ADDR = PathAddress.pathAddress(SYSTEM_PROPERTY, "kafka.bootstrap.servers");
+
+    private static final PathAddress MP_CONFIG_SOURCE_ADDR =
+            PathAddress.pathAddress(SUBSYSTEM, "microprofile-config-smallrye")
+                    .append("config-source", "testing-kafka");
+
 
     @Override
     public void setup(ManagementClient managementClient, String s) throws Exception {
@@ -30,7 +52,7 @@ public class RunKafkaSetupTask implements ServerSetupTask {
             throw new IllegalArgumentException("Specify Kafka version with -Dwildfly.test.kafka.version");
         }
         container = new KafkaContainer("apache/kafka-native:" + kafkaVersion);
-        container.setPortBindings(Arrays.asList("9092:9092", "9093:9093"));
+        container.setExposedPorts(List.of(9092, 9093));
 
         for (Map.Entry<String, String> entry : extraBrokerProperties().entrySet()) {
             container.addEnv(entry.getKey(), entry.getValue());
@@ -38,7 +60,7 @@ public class RunKafkaSetupTask implements ServerSetupTask {
 
         container.start();
 
-        companion = new KafkaCompanion("INTERNAL://localhost:9092");
+        companion = new KafkaCompanion("INTERNAL://localhost:" + container.getMappedPort(9092));
 
         Map<String, Integer> topicsAndPartitions = getTopicsAndPartitions();
         if (topicsAndPartitions == null || topicsAndPartitions.isEmpty()) {
@@ -48,10 +70,16 @@ public class RunKafkaSetupTask implements ServerSetupTask {
                 companion.topics().createAndWait(entry.getKey(), entry.getValue(), Duration.of(10, ChronoUnit.SECONDS));
             }
         }
+        addKafkaPortPropertyToModel(managementClient, container, 9092);
     }
 
     @Override
     public void tearDown(ManagementClient managementClient, String s) throws Exception {
+        removeKafkaPortPropertyFromModel(managementClient);
+        ModelNode result = managementClient.getControllerClient().execute(Util.createOperation(READ_RESOURCE_OPERATION, KAFKA_PORT_PROPERTY_ADDR));
+        if (SUCCESS.equals(result.get(OUTCOME).asString())) {
+            managementClient.getControllerClient().execute(Util.createRemoveOperation(KAFKA_PORT_PROPERTY_ADDR));
+        }
         if (companion != null) {
             try {
                 companion.close();
@@ -68,7 +96,39 @@ public class RunKafkaSetupTask implements ServerSetupTask {
         }
     }
 
+    public static void addKafkaPortPropertyToModel(ManagementClient client, GenericContainer<?> container, int mappedPort) throws IOException {
+        int mapped = container.getMappedPort(mappedPort);
+        ModelNode result = client.getControllerClient().execute(
+                Util.createAddOperation(KAFKA_PORT_PROPERTY_ADDR, Map.of(VALUE, new ModelNode(mapped))));
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
 
+//        result = client.getControllerClient().execute(
+//                Util.createAddOperation(BOOTSTRAP_SERVERS_PROPERTY_ADDR, Map.of(VALUE, new ModelNode("localhost:" + mapped + ""))));
+//        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+//
+//        ModelNode op = Util.createAddOperation(MP_CONFIG_SOURCE_ADDR);
+//        op.get(PROPERTIES).add(KAFKA_PORT, new ModelNode(mapped));
+//        result = client.getControllerClient().execute(op);
+//        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+
+    }
+
+    public static void removeKafkaPortPropertyFromModel(ManagementClient client) throws IOException {
+        ModelNode result = client.getControllerClient().execute(Util.createOperation(READ_RESOURCE_OPERATION, KAFKA_PORT_PROPERTY_ADDR));
+        if (SUCCESS.equals(result.get(OUTCOME).asString())) {
+            client.getControllerClient().execute(Util.createRemoveOperation(KAFKA_PORT_PROPERTY_ADDR));
+        }
+
+//        result = client.getControllerClient().execute(Util.createOperation(READ_RESOURCE_OPERATION, BOOTSTRAP_SERVERS_PROPERTY_ADDR));
+//        if (SUCCESS.equals(result.get(OUTCOME).asString())) {
+//            client.getControllerClient().execute(Util.createRemoveOperation(BOOTSTRAP_SERVERS_PROPERTY_ADDR));
+//        }
+//
+//        result = client.getControllerClient().execute(Util.createOperation(READ_RESOURCE_OPERATION, MP_CONFIG_SOURCE_ADDR));
+//        if (SUCCESS.equals(result.get(OUTCOME).asString())) {
+//            client.getControllerClient().execute(Util.createRemoveOperation(MP_CONFIG_SOURCE_ADDR));
+//        }
+    }
 
     protected Map<String, String> extraBrokerProperties() {
         return Collections.emptyMap();
--- testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/messaging/kafka/ssl/RunKafkaWithSslSetupTask.java
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/messaging/kafka/ssl/RunKafkaWithSslSetupTask.java
@@ -6,6 +6,8 @@
 package org.wildfly.test.integration.microprofile.reactive.messaging.kafka.ssl;
 
 import static org.wildfly.test.integration.microprofile.reactive.KeystoreUtil.SERVER_KEYSTORE_PATH;
+import static org.wildfly.test.integration.microprofile.reactive.RunKafkaSetupTask.addKafkaPortPropertyToModel;
+import static org.wildfly.test.integration.microprofile.reactive.RunKafkaSetupTask.removeKafkaPortPropertyFromModel;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -35,46 +37,82 @@ public class RunKafkaWithSslSetupTask implements ServerSetupTask {
     public void setup(ManagementClient managementClient, String s) throws Exception {
         try {
             KeystoreUtil.createKeystores();
-            String kafkaVersion = WildFlySecurityManager.getPropertyPrivileged("wildfly.test.kafka.version", null);
-            if (kafkaVersion == null) {
-                throw new IllegalArgumentException("Specify Kafka version with -Dwildfly.test.kafka.version");
+
+            try {
+                createContainer();
+            } catch (Exception e) {
+                // On Podman on Mac this does not always work the first time.
+                // Try again
+                Throwable curr = e;
+                int i = 0;
+                boolean restart = false;
+                while (i < 15) {
+                    i++;
+                    if (curr.getClass().getSimpleName().equals("NoHttpResponseException")) {
+                        restart = true;
+                        break;
+                    }
+                    if (curr.getCause() == null) {
+                        break;
+                    }
+                    curr = curr.getCause();
+                }
+                if (restart) {
+                    if (container != null) {
+                        container.stop();
+                    }
+                    Thread.sleep(5000);
+                    createContainer();
+                }
             }
 
-            // The KafkaContainer class doesn't play nicely when trying to make it use SSL
-            container = new GenericContainer("apache/kafka-native:" + kafkaVersion);
-            container.setPortBindings(Arrays.asList("9092:9092", "19092:19092"));
-            container.withCopyFileToContainer(
-                    MountableFile.forHostPath(Path.of("src/test/resources/org/wildfly/test/integration/microprofile/reactive/messaging/kafka/ssl/server.properties")),
-                    "/mnt/shared/config/server.properties"
-            );
-            container.waitingFor(Wait.forLogMessage(".*Transitioning from RECOVERY to RUNNING.*", 1));
 
-
-            // Copy the keystore files to the expected container location
-            container.withCopyFileToContainer(
-                    MountableFile.forHostPath(SERVER_KEYSTORE_PATH.getParent()),
-                    "/etc/kafka/secrets/");
-
-//            // Set env vars which don't seem to have any effect when only in server.properties
-            container.addEnv("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@localhost:29093");
-
-            container.start();
-
-            companion = new KafkaCompanion("INTERNAL://localhost:19092");
+            companion = new KafkaCompanion("INTERNAL://localhost:" + container.getMappedPort(19092));
             companion.topics().createAndWait("testing", 1, Duration.of(10, ChronoUnit.SECONDS));
+
+            addKafkaPortPropertyToModel(managementClient, container, 9092);
         } catch (Exception e) {
-            cleanupKafka();
+            cleanupKafka(managementClient);
             throw e;
         }
     }
 
-    @Override
-    public void tearDown(ManagementClient managementClient, String s) throws Exception {
-        cleanupKafka();
+    private void createContainer() {
+        String kafkaVersion = WildFlySecurityManager.getPropertyPrivileged("wildfly.test.kafka.version", null);
+        if (kafkaVersion == null) {
+            throw new IllegalArgumentException("Specify Kafka version with -Dwildfly.test.kafka.version");
+        }
+
+        // The KafkaContainer class doesn't play nicely when trying to make it use SSL
+        container = new GenericContainer("apache/kafka-native:" + kafkaVersion);
+        container.setPortBindings(Arrays.asList("9092:9092", "19092:19092"));
+        container.withCopyFileToContainer(
+                MountableFile.forHostPath(Path.of("src/test/resources/org/wildfly/test/integration/microprofile/reactive/messaging/kafka/ssl/server.properties")),
+                "/mnt/shared/config/server.properties"
+        );
+        container.waitingFor(Wait.forLogMessage(".*Transitioning from RECOVERY to RUNNING.*", 1));
+
+
+        // Copy the keystore files to the expected container location
+        container.withCopyFileToContainer(
+                MountableFile.forHostPath(SERVER_KEYSTORE_PATH.getParent()),
+                "/etc/kafka/secrets/");
+
+        // Set env vars which don't seem to have any effect when only in server.properties
+        container.addEnv("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@localhost:29093");
+
+        container.start();
+
     }
 
-    private void cleanupKafka() throws IOException {
+    @Override
+    public void tearDown(ManagementClient managementClient, String s) throws Exception {
+        cleanupKafka(managementClient);
+    }
+
+    private void cleanupKafka(ManagementClient client) throws IOException {
         try {
+            removeKafkaPortPropertyFromModel(client);
             if (companion != null) {
                 try {
                     companion.close();
