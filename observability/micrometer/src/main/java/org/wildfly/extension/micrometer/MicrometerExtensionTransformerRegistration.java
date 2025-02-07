3cea68d0c77bea86d58c42a8a195652ab17c0c3a
Jason Lee <jason@steeplesoft.com>
2025-02-07 16:52:46 -0600

    [WFLY-18582-new] Add prometheus endpoint to micrometer extension
    Add prometheus support
    Create new io.prometheus module
    Add tests for Prometheus context
    Update documentation
    Make ServerSetupTasks more resilient against duplicate resource errors
    Make sure OTLP is properly configured every time

--- observability/micrometer/src/main/java/org/wildfly/extension/micrometer/MicrometerExtensionTransformerRegistration.java
+++ observability/micrometer/src/main/java/org/wildfly/extension/micrometer/MicrometerExtensionTransformerRegistration.java
@@ -4,13 +4,18 @@
  */
 package org.wildfly.extension.micrometer;
 
+import static org.wildfly.extension.micrometer.MicrometerSubsystemModel.VERSION_1_1_0;
+
+import org.jboss.as.controller.ModelVersion;
 import org.jboss.as.controller.transform.ExtensionTransformerRegistration;
 import org.jboss.as.controller.transform.SubsystemTransformerRegistration;
+import org.jboss.as.controller.transform.description.AttributeConverter;
 import org.jboss.as.controller.transform.description.ChainedTransformationDescriptionBuilder;
 import org.jboss.as.controller.transform.description.ResourceTransformationDescriptionBuilder;
 import org.jboss.as.controller.transform.description.TransformationDescriptionBuilder;
 import org.kohsuke.MetaInfServices;
 import org.wildfly.extension.micrometer.otlp.OtlpRegistryDefinitionRegistrar;
+import org.wildfly.extension.micrometer.prometheus.PrometheusRegistryDefinitionRegistrar;
 
 @MetaInfServices
 public class MicrometerExtensionTransformerRegistration implements ExtensionTransformerRegistration {
@@ -21,15 +26,21 @@ public class MicrometerExtensionTransformerRegistration implements ExtensionTran
 
     @Override
     public void registerTransformers(SubsystemTransformerRegistration registration) {
-        ChainedTransformationDescriptionBuilder builder =
-                TransformationDescriptionBuilder.Factory.createChainedSubystemInstance(
-                        registration.getCurrentSubsystemVersion());
+        final ModelVersion currentModel = registration.getCurrentSubsystemVersion();
+        ChainedTransformationDescriptionBuilder chainedBuilder =
+                TransformationDescriptionBuilder.Factory.createChainedSubystemInstance(currentModel);
 
-        registerV_2_0_Transformers(builder.createBuilder(MicrometerSubsystemModel.VERSION_2_0_0.getVersion(),
-                MicrometerSubsystemModel.VERSION_1_1_0.getVersion()));
+        // 2.0.0_Community (WildFly 36) to 1.1.0 (WildFly 33)
+        from2(chainedBuilder.createBuilder(currentModel, VERSION_1_1_0.getVersion()));
+
+        chainedBuilder.buildAndRegister(registration, new ModelVersion[]{VERSION_1_1_0.getVersion()});
     }
 
-    private void registerV_2_0_Transformers(ResourceTransformationDescriptionBuilder builder) {
+    private void from2(ResourceTransformationDescriptionBuilder builder) {
         builder.addChildRedirection(OtlpRegistryDefinitionRegistrar.PATH, MicrometerSubsystemRegistrar.PATH);
+        builder.getAttributeBuilder()
+            .setValueConverter(AttributeConverter.DEFAULT_VALUE, MicrometerSubsystemRegistrar.STEP)
+            .end();
+        builder.rejectChildResource(PrometheusRegistryDefinitionRegistrar.PATH);
     }
 }
